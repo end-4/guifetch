e46437512852be511484f1187e4b78ecc90b105b
end-4 <97237370+end-4@users.noreply.github.com>
2023-06-29 15:18:13 +0700

    endeavouros

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
       title: 'Guifetch',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        fontFamily: 'Lexend',
         scaffoldBackgroundColor: Colors.transparent,
         textTheme: const TextTheme(
           bodyText2: TextStyle(),
@@ -93,147 +94,157 @@ class MyHomePage extends HookConsumerWidget {
               error: (err, trace) => const Text("Error"),
               loading: () => const Center(),
               data: (logo) => config.when(
-                error: (err, trace) => const Text("Error"),
-                loading: () => const Center(),
-                data: (config) {
-                  return Container(
-                    color: config.backgroundColor,
-                    // decoration: BoxDecoration(
-                    //   gradient: LinearGradient(
-                    //     begin: Alignment.topCenter,
-                    //     end: Alignment.bottomCenter,
-                    //     colors: [
-                    //       colors.vibrantColor?.color.withOpacity(0.1) ??
-                    //           colors.paletteColors.first.color.withOpacity(0.1),
-                    //       Colors.transparent,
-                    //     ],
-                    //   ),
-                    // ),
-                    child: Stack(
-                      children: [
-                        AnimatedBuilder(
-                            animation: scrollProgress,
-                            builder: (context, snapshot) {
-                              return Stack(
-                                children: [
-                                  Column(
-                                    children: [
-                                      const SizedBox(height: 20),
-                                      if (logo != null)
-                                        Center(
-                                          child: DropShadowImage(
-                                            image: Image(
-                                              image: logo,
-                                              height:
-                                                  150 * (1 - scrollProgress.value),
-                                              filterQuality: ui.FilterQuality.high,
-                                            ),
-                                            blurRadius: 40,
-                                            offset: const Offset(0, 0),
-                                          ),
-                                        ),
-                                    ],
-                                  ),
-                                  Flex(
-                                    direction: Axis.horizontal,
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: <Widget>[
-                                      Container(
-                                        decoration: BoxDecoration(
-                                          color: containerColor,
-                                          borderRadius: BorderRadius.only(
-                                            bottomRight: const Radius.circular(20),
-                                            bottomLeft: Radius.circular(
-                                              20 * scrollProgress.value,
-                                            ),
-                                          ),
-                                        ),
-                                        height: 40,
-                                        padding: const EdgeInsets.symmetric(
-                                          horizontal: 20,
-                                          vertical: 10,
-                                        ),
-                                        constraints: BoxConstraints(
-                                          minWidth:
-                                              MediaQuery.of(context).size.width *
-                                                  scrollProgress.value,
-                                        ),
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            Text(infoTitle),
-                                            if (logo != null)
-                                              Opacity(
-                                                opacity: scrollProgress.value,
-                                                child: Image(
-                                                  image: logo,
-                                                  height: 20 * scrollProgress.value,
-                                                  filterQuality:
-                                                      ui.FilterQuality.medium,
-                                                ),
+                  error: (err, trace) => const Text("Error"),
+                  loading: () => const Center(),
+                  data: (config) {
+                    return Container(
+                      color: const Color(0x991A1513),
+                      // decoration: BoxDecoration(
+                      //   gradient: LinearGradient(
+                      //     begin: Alignment.topCenter,
+                      //     end: Alignment.bottomCenter,
+                      //     colors: [
+                      //       colors.vibrantColor?.color.withOpacity(0.1) ??
+                      //           colors.paletteColors.first.color.withOpacity(0.1),
+                      //       Colors.transparent,
+                      //     ],
+                      //   ),
+                      // ),
+                      child: Stack(
+                        children: [
+                          AnimatedBuilder(
+                              animation: scrollProgress,
+                              builder: (context, snapshot) {
+                                return Stack(
+                                  children: [
+                                    Column(
+                                      children: [
+                                        const SizedBox(height: 20),
+                                        if (logo != null)
+                                          Center(
+                                            child: DropShadowImage(
+                                              image: Image(
+                                                image: logo,
+                                                height: 150 *
+                                                    (1 - scrollProgress.value),
+                                                filterQuality:
+                                                    ui.FilterQuality.high,
                                               ),
-                                          ],
+                                              blurRadius: 40,
+                                              offset: const Offset(0, 0),
+                                            ),
+                                          ),
+                                      ],
+                                    ),
+                                    Flex(
+                                      direction: Axis.horizontal,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: <Widget>[
+                                        Container(
+                                          decoration: BoxDecoration(
+                                            color: containerColor,
+                                            borderRadius: BorderRadius.only(
+                                              bottomRight:
+                                                  const Radius.circular(20),
+                                              bottomLeft: Radius.circular(
+                                                20 * scrollProgress.value,
+                                              ),
+                                            ),
+                                          ),
+                                          height: 40,
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 20,
+                                            vertical: 10,
+                                          ),
+                                          constraints: BoxConstraints(
+                                            minWidth: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                scrollProgress.value,
+                                          ),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(infoTitle,
+                                                  style: TextStyle(
+                                                    fontSize: 20,
+                                                    color:
+                                                        const Color(0xFFE4B4FF),
+                                                  )),
+                                              if (logo != null)
+                                                Opacity(
+                                                  opacity: scrollProgress.value,
+                                                  child: Image(
+                                                    image: logo,
+                                                    height: 20 *
+                                                        scrollProgress.value,
+                                                    filterQuality:
+                                                        ui.FilterQuality.medium,
+                                                  ),
+                                                ),
+                                            ],
+                                          ),
                                         ),
-                                      ),
-                                    ],
+                                      ],
+                                    ),
+                                  ],
+                                );
+                              }),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 45),
+                            child: ImprovedScrolling(
+                              scrollController: scrollController,
+                              enableMMBScrolling: true,
+                              enableKeyboardScrolling: true,
+                              enableCustomMouseWheelScrolling: true,
+                              keyboardScrollConfig: KeyboardScrollConfig(
+                                arrowsScrollAmount: 250.0,
+                                homeScrollDurationBuilder:
+                                    (currentScrollOffset, minScrollOffset) {
+                                  return const Duration(milliseconds: 100);
+                                },
+                                endScrollDurationBuilder:
+                                    (currentScrollOffset, maxScrollOffset) {
+                                  return const Duration(milliseconds: 2000);
+                                },
+                              ),
+                              // customMouseWheelScrollConfig:
+                              //     const CustomMouseWheelScrollConfig(
+                              //   scrollAmountMultiplier: 2.0,
+                              // ),
+                              child: SingleChildScrollView(
+                                controller: scrollController,
+                                physics: const NeverScrollableScrollPhysics(),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                    top: 8 + 150 - 20,
+                                    left: 8,
+                                    right: 8,
                                   ),
-                                ],
-                              );
-                            }),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 45),
-                          child: ImprovedScrolling(
-                            scrollController: scrollController,
-                            enableMMBScrolling: true,
-                            enableKeyboardScrolling: true,
-                            enableCustomMouseWheelScrolling: true,
-                            keyboardScrollConfig: KeyboardScrollConfig(
-                              arrowsScrollAmount: 250.0,
-                              homeScrollDurationBuilder:
-                                  (currentScrollOffset, minScrollOffset) {
-                                return const Duration(milliseconds: 100);
-                              },
-                              endScrollDurationBuilder:
-                                  (currentScrollOffset, maxScrollOffset) {
-                                return const Duration(milliseconds: 2000);
-                              },
-                            ),
-                            // customMouseWheelScrollConfig:
-                            //     const CustomMouseWheelScrollConfig(
-                            //   scrollAmountMultiplier: 2.0,
-                            // ),
-                            child: SingleChildScrollView(
-                              controller: scrollController,
-                              physics: const NeverScrollableScrollPhysics(),
-                              child: Padding(
-                                padding: const EdgeInsets.only(
-                                  top: 8 + 150 - 20,
-                                  left: 8,
-                                  right: 8,
-                                ),
-                                child: Container(
-                                  padding: const EdgeInsets.all(20),
-                                  decoration: BoxDecoration(
-                                    color: containerColor,
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-                                  child: Column(
-                                    children: infoFields
-                                        .map((info) => Field(
-                                            info: info, titleColor: titleColor))
-                                        .toList(),
+                                  child: Container(
+                                    padding: const EdgeInsets.all(20),
+                                    decoration: BoxDecoration(
+                                      color: containerColor,
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    child: Column(
+                                      children: infoFields
+                                          .map((info) => Field(
+                                              info: info,
+                                              titleColor: titleColor))
+                                          .toList(),
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  );
-                }
-              ),
+                        ],
+                      ),
+                    );
+                  }),
             );
           },
         ),
@@ -257,9 +268,18 @@ class Field extends HookConsumerWidget {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(info.title, style: TextStyle(color: titleColor ?? Colors.blue)),
+        Text(info.title,
+            style: TextStyle(
+              fontSize: 20,
+              color: const Color(0xFFF18D8C),
+            )),
         const Spacer(),
-        Text(info.text, textAlign: TextAlign.end),
+        Text(info.text,
+            textAlign: TextAlign.end,
+            style: TextStyle(
+              fontSize: 20,
+              color: const Color(0xFFADC6FF),
+            )),
       ],
     );
   }
